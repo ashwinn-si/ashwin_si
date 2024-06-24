285b9a88546de33e02694c5d4c182e888927cf93
ashwinn-si <ashwin23cse@gmail.com>
2024-06-24 10:43:45 +0530

    timer_settings

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
-window.addEventListener('load', function() {
+function myFunction() {
     const loaderContainer = document.querySelector('.loader-container');
     const content = document.querySelector('.main');
 
     loaderContainer.style.display = 'none';
     content.classList.remove('hidden');
-});
+}
+
+// Pass the function reference to setTimeout
+setTimeout(myFunction, 5000);
