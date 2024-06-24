f721b8a054d5f43a2cfc19e6451aa2b1a66aaf85
ashwinn-si <ashwin23cse@gmail.com>
2024-06-24 10:48:55 +0530

    Done....!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 function myFunction() {
     const loaderContainer = document.querySelector('.loader-container');
-    const content = document.querySelector('.main');
 
     loaderContainer.style.display = 'none';
-    content.classList.remove('hidden');
 }
-
-// Pass the function reference to setTimeout
 setTimeout(myFunction, 5000);
